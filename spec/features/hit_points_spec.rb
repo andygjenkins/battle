823bd2e542c87a6c337e03e058653a8b993d25b6
andy jenkins <andygjenkins@gmail.com>
2016-10-11 15:11:33 +0100

    DRY out feature tests

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,14 +2,8 @@ require 'spec_helper'
 
 feature 'Hit Points' do
 
-  before do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
-  end
-
   scenario 'checking names' do
+    sign_in_and_play
     expect(page).to have_content 'Mittens: 50HP'
   end
 
