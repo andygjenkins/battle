3024ea1bfa14a44459ed80ac58c03f5684364314
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-11 16:00:41 +0100

    added attack feature and tested

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'Attack' do
+  scenario 'attacking player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Dave attacked Mittens'
+  end
+end
