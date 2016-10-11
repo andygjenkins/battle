68296723eef3bdf918d6884f639bc78884c5e8b5
andy jenkins <andygjenkins@gmail.com>
2016-10-11 12:19:45 +0100

    Add feature tests with capybara

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require 'sinatra/base'
 
-class battle < Sinatra::Base
+class Battle < Sinatra::Base
   get '/' do
-    'Hello battle!'
+    erb :index
   end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+
+
+
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
