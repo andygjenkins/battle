829321ce229552dd443acba2e995d556b4e24d07
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-11 15:02:43 +0100

    passed test for Player 2 HP

--- app.rb
+++ app.rb
@@ -9,14 +9,15 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    session['player_1_name'] = params[:player_1_name]
+    session['player_2_name'] = params[:player_2_name]
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = session['player_1_name']
+    @player_2_name = session['player_2_name']
+    @p2hp = 50
     erb :play
   end
 
