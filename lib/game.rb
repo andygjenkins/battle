a84397d00827739dddaa2595a09eff32f40225d1
andy jenkins <andygjenkins@gmail.com>
2016-10-13 08:23:55 +0100

    Add back in cofusing code

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,13 @@
 
 class Game
 
+  # attr_reader :player_1, :player_2
+  #
+  # def initialize(player_1, player_2)
+  #   @player_1 = player_1
+  #   @player_2 = player_2
+  # end
+
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
   end
